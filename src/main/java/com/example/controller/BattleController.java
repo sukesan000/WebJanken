9979bccceb0ea922d1fc943e63fc61881df1e473
sukesan000 <oyama.k.1997@gmail.com>
2020-10-30 21:47:22 +0900

    ラジオボタン導入

--- src/main/java/com/example/controller/BattleController.java
+++ src/main/java/com/example/controller/BattleController.java
@@ -1,12 +1,19 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class BattleController {
-	@GetMapping("html//battle")
+	@RequestMapping(value="/html/battle", method=RequestMethod.GET)
 	public String GetBattle() {
 		return "html/battle";
 	}
+	
+//	@RequestMapping(value = "/battle", method = RequestMethod.POST)
+//	  public ModelAndView send(@RequestParam(value="radio1",required=false)String radio1, Model mav) {
+//	   
+//	    return "html/battle";
+//	  }
 }
