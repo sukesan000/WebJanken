30b4bb256e61401c98d331b9eb2c8fa79694c2ca
sukesan000 <oyama.k.1997@gmail.com>
2020-10-30 23:03:24 +0900

    クリックボタンを押すと選んだ手が出力される

--- src/main/java/com/example/controller/BattleController.java
+++ src/main/java/com/example/controller/BattleController.java
@@ -1,19 +1,23 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class BattleController {
-	@RequestMapping(value="/html/battle", method=RequestMethod.GET)
+	@GetMapping(value="/html/battle")
 	public String GetBattle() {
 		return "html/battle";
 	}
 	
-//	@RequestMapping(value = "/battle", method = RequestMethod.POST)
-//	  public ModelAndView send(@RequestParam(value="radio1",required=false)String radio1, Model mav) {
-//	   
-//	    return "html/battle";
-//	  }
+	@PostMapping(value="/html/battle")
+	public ModelAndView send(@RequestParam(value="radio1")String radio1, ModelAndView model) {
+		String res = "";
+		res = "なに出した？     " + radio1;
+		model.addObject("msg", res);
+		return model;
+	}
 }
